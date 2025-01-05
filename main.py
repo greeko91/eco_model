458beb1fbffe327725ad274cb2b90496ead4ca65
Andrey Piskunov <andrey91y@yandex.ru>
2025-01-05 15:15:27 +0300

    last commit

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pandas as pd
 #import netCDF4
 import nctoolkit as nc
+from multiprocessing import freeze_support, Process
+
+
+def worker():
+    nc_name = 'dutch_harbor_1_mhw_2006.nc'
+    ds = nc.open_data(nc_name)
+    print(ds)
+    df = ds.to_dataframe()
+    print(df)
+    
+
 
 if __name__ == "__main__":
-    nc_name = 'dutch_harbor_1_mhw_2006.nc'
+    freeze_support()
+    p = Process(target=worker)
+    p.start()
+    p.join()
 
-    ds = nc.open_data(nc_name)
-
-#print(ds)
-
-#df = ds.to_dataframe()
-
-#print(df)
